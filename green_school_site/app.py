5f48737578fd8553a1da7e352994bc0f9df8461e
Ainazik <anzk04.2@gmail.com>
2026-05-20 16:31:51 +0600

    fix css path

--- green_school_site/app.py
+++ green_school_site/app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # CSS
-with open("style.css") as f:
+with open("./green_school_site/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Navbar
